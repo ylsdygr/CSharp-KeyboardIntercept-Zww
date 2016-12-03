a4306eff72d14197cc220a6c50a4841eba94ab89
Colin Jack <245655340@qq.com>
2016-12-03 11:19:50 +0800

    2016.10.20_Add Functions
    1.增加服务器端（以本地磁盘替代）授权码列表文件的读取方法
    2.增加授权码验证及授权码更新写入远程服务器及U盘方法

--- KeyboardIntercept/HookHelper.cs
+++ KeyboardIntercept/HookHelper.cs
@@ -217,6 +217,7 @@ namespace KeyboardIntercept
         private const int WM_MBUTTONDBLCLK = 0x209;
         private int MouseHookProc(int nCode, Int32 wParam, IntPtr lParam)
         {
+            return CallNextHookEx(_hMouseHook, nCode, wParam, lParam);
             if ((nCode >= 0) && (OnMouseActivity != null))
             {
                 MouseButtons button = MouseButtons.None;
@@ -269,31 +270,36 @@ namespace KeyboardIntercept
         private const int WM_SYSKEYDOWN = 0x104;
         private const int WM_SYSKEYUP = 0x105;
         
-        int count = 0;
         //以下两个定义的标记在U盘拨出时会清除。
         int cutOrNot = 0;
         int judgedOrNot = 0;//是否对U盘中的授权文件进行对比，0未对比，1对比过
         string[] comparedKeys = { };
+        string paraConstantInformations = "ConstantInformations";//固定的那串信息
+        string para_netFilePath = "E:\\1022\\authorizedKeys";
+        string para_uFilePath = "E:\\1022\\authorizedKey";
+
         private int KeyboardHookProc(int nCode, Int32 wParam, IntPtr lParam)
         {
             //if (nCode >= 0 && wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
             if (nCode >= 0 && wParam == WM_KEYDOWN)
             {
+                
                 KeyboardHookStruct MyKeyboardHookStruct = (KeyboardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyboardHookStruct));
                 Keys keyData = (Keys)MyKeyboardHookStruct.vkCode;
                 if (judgedOrNot == 0)//未对比的话就对比一次
                 {
-                    string[] passwdArray = this.readAndOutputKeysIn("C:\\Users\\Two\\Desktop\\authorizedKeys");
-                    string passwdKey = this.readKeyFromU("C:\\Users\\Two\\Desktop\\authorizedKey");
+                    string[] passwdArray = this.readAndOutputKeysIn(para_netFilePath);
+                    string passwdKey = this.readKeyFromU(para_uFilePath);
                     foreach (string item in passwdArray)
                     {
                         if (string.Equals(item, passwdKey, StringComparison.CurrentCulture))
                         {
                             cutOrNot = 1;
-                            Stop();
+                            //Stop();
+                            this.updateKeyToUAndAuthorized(para_netFilePath, para_uFilePath);
                         }
                     }
-                    judgedOrNot = 1;//标记已对比过
+                    //judgedOrNot = 1;//标记已对比过
                     //以下的判断针对第一次对比授权的结果选择第一次的按键是否正常发送
                     if (cutOrNot == 0){
                         SendKeys.Send("");
@@ -303,8 +309,8 @@ namespace KeyboardIntercept
                         return CallNextHookEx(_hKeyboardHook, nCode, wParam, lParam);
                     }
                 }
-                    //如果已对比过，则根据对比结果来判断是否要将以后的按键发送。
-                else {
+                else
+                { //如果已对比过，则根据对比结果来判断是否要将以后的按键发送。
                     if (cutOrNot == 0)
                     {
                         SendKeys.Send("");
@@ -320,12 +326,12 @@ namespace KeyboardIntercept
         }
         
         //授权记录生成器
-        private string authorizedKeysGenerator(){
+        private string authorizedKeysGenerator(string ConstantInformations, int calcCount)
+        {
             System.DateTime currentDate = new DateTime();
-            int count = 38;
             string authorizedKey = String.Empty;
             authorizedKey = randomValuesGenerator(20);
-            authorizedKey += "ConstantInformations";
+            authorizedKey += ConstantInformations;
             authorizedKey += randomValuesGenerator(8);
             authorizedKey += System.DateTime.Now.Date.Year.ToString();
             authorizedKey += randomValuesGenerator(8);
@@ -334,10 +340,9 @@ namespace KeyboardIntercept
             authorizedKey += System.DateTime.Now.Date.Day.ToString();
             authorizedKey += randomValuesGenerator(8);
             authorizedKey += "0x";
-            authorizedKey += count.ToString("x2");
+            authorizedKey += calcCount.ToString("x2");
             authorizedKey += randomValuesGenerator(14);
             authorizedKey += this.calculateCheckCode(authorizedKey).ToString();
-            System.Console.WriteLine(authorizedKey);
             
             return authorizedKey;
         }
@@ -373,7 +378,7 @@ namespace KeyboardIntercept
             checkCode = (char)((calcCheckSum % 26)+65);
             return checkCode;
         }   
-        //以每次读取一行的形式读取授权码
+        //以每次读取一行的形式读取授权码,返回字符数组
         private string[] readAndOutputKeysIn(string filePath)
         {
             ArrayList readedKeysList = new ArrayList();
@@ -392,6 +397,27 @@ namespace KeyboardIntercept
             }
             string[] readedKeys = (string[])readedKeysList.ToArray(typeof(string));
             return readedKeys;
+        }
+        //以每次读取一行的形式读取授权码,返回ArrayList
+        private ArrayList readAndOutputKeysInArrayList(string filePath)
+        {
+            ArrayList readedKeysList = new ArrayList();
+            try
+            {
+                FileStream keysInFile = new FileStream(filePath, FileMode.Open);
+                using (var stream = new StreamReader(keysInFile))
+                {
+                    while (!stream.EndOfStream)
+                    {
+                        readedKeysList.Add(stream.ReadLine());
+                    }
+                }
+                keysInFile.Close();
+            }
+            catch (IOException e){
+                //System.Console.WriteLine(e.ToString());
+            }
+            return readedKeysList;
         }
         //读取授权U盘中的一行授权码
         private string readKeyFromU(string filePath)
@@ -413,6 +439,77 @@ namespace KeyboardIntercept
                 //System.Console.WriteLine(e.ToString());
             }
             return readedKeys;
+        }
+        //产生新的授权码存储在U盘中与授权码文件中
+        private int updateKeyToUAndAuthorized(string netFilePath,string uFilePath) {
+            //当前U盘中的授权码
+            string currentKey = this.readKeyFromU(uFilePath);
+            //准备新生成授权码中的次数控制
+            string stringcount = currentKey.Substring(82, 2);
+            int keysUsedCount = Convert.ToInt32(stringcount, 16);            
+            if (keysUsedCount == 255)
+            {
+                keysUsedCount = 0;
+            }
+            else {
+                keysUsedCount += 1;
+            }
+            //新生成一串授权码
+            string outputString = this.authorizedKeysGenerator(paraConstantInformations, keysUsedCount);
+            int isUSuccessed = 0;//U盘是否写入成功
+            int isNetSuccessed = 0;//授权文件是否写入成功
+            try {
+                FileStream outputToU = new FileStream(uFilePath, FileMode.Open);
+                using (var stream = new StreamWriter(outputToU)){
+                    stream.Write(outputString);
+                }
+                outputToU.Close();
+                isUSuccessed = 1;
+            }catch(IOException e){
+                isUSuccessed = 0;
+            }
+            if(isUSuccessed == 1){//U盘写入成功后写入授权文件
+                ArrayList readedKeysList = new ArrayList();
+                readedKeysList = this.readAndOutputKeysInArrayList(netFilePath);
+                readedKeysList.Add(outputString);
+                for (int i = 0; i < readedKeysList.Count; i++){
+                    if (string.Equals(readedKeysList[i].ToString(), currentKey, StringComparison.CurrentCulture))
+                    {
+                        readedKeysList.RemoveAt(i);
+                    }
+                }
+                try{
+
+                    FileStream outputToNet = new FileStream(netFilePath, FileMode.Open);
+                    using (var stream = new StreamWriter(outputToNet))
+                    {
+                        foreach (string item in readedKeysList)
+                        {
+                            stream.Write(item);
+                            stream.Write("\n");
+                        }
+                    }
+                    outputToNet.Close();
+                    isNetSuccessed = 1;
+                }
+                catch (IOException e)
+                {
+                    isNetSuccessed = 0;
+                }
+            }
+            if (isNetSuccessed == 0) {
+                try{
+                    FileStream recoverU = new FileStream(uFilePath, FileMode.Open);
+                    using (var stream = new StreamWriter(recoverU))
+                    {
+                        stream.Write(currentKey);
+                    }
+                    recoverU.Close();
+                }
+                catch (IOException e){
+                }
+            }
+            return 1;
         }
     }
 }
